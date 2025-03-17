d5f6c41ac9fe66e3a6f740369d487680390871d9
Salman Al-Muhli <129013374+alhobishi22@users.noreply.github.com>
2025-03-18 01:48:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -74,6 +74,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'  # تغيير اسم الجدول من "user" إلى "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -89,7 +90,7 @@ class User(UserMixin, db.Model):
 
 class Report(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     type = db.Column(db.String(20), nullable=False)  # scammer, debt
     debt_amount = db.Column(db.Float)
     debt_date = db.Column(db.Date)
